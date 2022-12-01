734e1483bf5bd6aa9ea4facf2a80635b828b6eb9
Denis Vysokikh <denvys@primexbt.ch>
2022-12-01 11:18:06 +0300

    Day 1 of AoC 2022

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
+    fun elfCalories(input: List<String>): MutableMap<Int, MutableList<Int>> {
+        var idx = 1
+        return input.fold(mutableMapOf()) { acc, calories ->
+            if (calories.isBlank()) {
+                idx++
+            } else {
+                acc.getOrPut(idx, ::mutableListOf).add(calories.toInt())
+            }
+            acc
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val caloriesPerElf = elfCalories(input)
+        return caloriesPerElf.values.maxOfOrNull { it.sum() } ?: -1
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val caloriesPerElf = elfCalories(input)
+        val caloriesSums = caloriesPerElf.mapValues { it.value.sum() }.values
+        return caloriesSums.sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
